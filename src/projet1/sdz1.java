398bbe862b4648f9bd3caf285bec80afa887505b
DidierF <didier.faudemer@gmail.com>
2015-02-03 21:54:14 +0100

    testa etetetetetetet

---
+++ src/projet1/sdz1.java
@@ -0,0 +1,14 @@
+package projet1;
+
+import java.util.Scanner;
+
+public class sdz1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un mot :");
+		String str = sc.nextLine();
+		System.out.println("Vous avez saisi : " + str);
+	}
+
+}
